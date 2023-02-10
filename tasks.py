19fa5a519a559e97fae46915bab1048457cc9329
Daniel <danielkarimov72541@gmail.com>
2023-02-10 21:23:47 +0300

    Add some tasks

--- tasks.py
+++ tasks.py
@@ -2,10 +2,14 @@ import datetime
 import random
 import string
 import os
+import csv
+import time
 
 
 # information output
 
+# 1. Вывести на экран текущее название дня недели, название
+# месяца и свое имя. Каждое слово должно быть в отдельной
 
 def task_one():
     weekdays = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
@@ -21,6 +25,9 @@ def task_one():
     print(f"RU:\n{ru_weekday}\n{ru_month}\nДаниель")
 
 
+# 2. Вывести на экран букву "W" из символов "*"
+# (звездочка).
+
 def task_two():
     symbols = ['*\t  *\t\t*', ' *   * *   *', '  * *   * *', '   *\t *']
     for s in symbols:
@@ -29,10 +36,16 @@ def task_two():
 
 # arithmetic
 
+# 3. Вычислите значение
+# выражения (a+4b)(a−3b)+a2 при a=2 и b=3. Ответ: -94
+
 def task_tree(a=2, b=3):
     answer = (a + 4 * b) * (a - 3 * b) + a ** 2
     print(f"Answer: {answer}")
 
+
+# 4. Пользователь вводит два числа. Найдите сумму и
+# произведение данных чисел.
 
 def task_four():
     nums = input("Enter a numbers: ").split(' ')
@@ -40,10 +53,16 @@ def task_four():
     print(f"Multiple: {float(nums[0]) * float(nums[1])}")
 
 
+# 5. Пользователь вводит число. Выведите на экран квадрат этого
+# числа, куб этого числа.
+
 def task_five():
     num = float(input("Enter a numbers: "))
     print(f"Square: {num ** 2}\nСube: {num ** 3}")
 
+
+# 6. Пользователь вводит время в минутах и расстояние в
+# километрах. Найдите скорость в м/c.
 
 def task_six():
     time = float(input("Enter a minutes: ")) * 60
@@ -51,11 +70,17 @@ def task_six():
     print(f"Speed: {distance / time:.5f}")
 
 
+# 7. Выведите на экран прямоугольник из нулей. Количество строк
+# вводит пользователь, количество столбцов равно 5.
+
 def task_seven():
     rows = int(input("Enter a numbers: "))
     for i in range(rows):
         print("0 0 0 0 0")
 
+
+# 8. Для данного n найти сумму 1+2+3+...+n. Например,
+# для n=10 ответ равен 55
 
 def task_eight():
     num = int(input("Enter a numbers: "))
@@ -65,11 +90,25 @@ def task_eight():
     print(f"Summ: {sum_for_n}")
 
 
+# 9. Выведите на экран строки (в последней строке n звездочек):
+# *
+# **
+# ***
+# ****
+# *****
+
 def task_nine():
     num = int(input("Enter a number: "))
     for i in range(num + 1):
         print("*" * i)
 
+
+# 10. Вывести на экран:
+# AAAAAAAAAAAAAAAA
+# ABBBBBBBBBBBBBBA
+# ABBBBBBBBBBBBBBA
+# ABBBBBBBBBBBBBBA
+# AAAAAAAAAAAAAAAA
 
 def task_ten():
     num = int(input("Enter a number: "))
@@ -78,6 +117,8 @@ def task_ten():
         print("A" + "B" * (num * 2 - 2) + "A")
     print("A" * num * 2)
 
+
+# 11. Вывести квадрат 7 на 7 из случайных букв.
 
 def task_eleven(row=7, col=7):
     ru_alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
@@ -96,12 +137,22 @@ def task_eleven(row=7, col=7):
         print()
 
 
+    # Medium
+
+# 1. Перевернуть число
+# Вводится целое число. Вывести число, обратное введенному по порядку
+# составляющих его цифр. Например, введено 3425, надо вывести 5243.
+
 def task_twelve():
     num = list(input("Enter a number: "))
     print("Reverse number: ", end='')
     for n in num[::-1]:
         print(n, end='')
 
+
+# 2. Посчитать четные и нечетные цифры числа
+# Определить, сколько в числе четных цифр, а сколько нечетных. Число вводится с
+# клавиатуры.
 
 def task_thirteen():
     try:
@@ -119,6 +170,12 @@ def task_thirteen():
         task_thirteen()
 
 
+# 3. Даны списки:
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].
+# Нужно вернуть список, который состоит из элементов, общих для этих двух
+# списков.
+
 def task_fourteen():
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -129,27 +186,42 @@ def task_fourteen():
     print(common_numbers)
 
 
+# 4. Выбрать из строки числа
+# Дана строка, содержащая натуральные числа и слова. Необходимо сформировать
+# список из чисел, содержащихся в этой строке. Например, задана строка "abc83 cde7
+# 1 b 24". На выходе мы должны получить список [83, 7, 1, 24].
+
 def task_fifteen():
     pass
+    # Не придумал как сделать без регулярного выражения
 
 
-def task_sixteen():
+# 5. Посчитать количество простых чисел
+# Пользователь вводит диапазон в пределе которого необходимо посчитать все
+# простые числа
+
+def search_simp_nums():
     nums = input("Enter a numbers: ").split(' ')
-    simp_num = []
     start = int(nums[0])
     end = int(nums[1])
-    for i in range(start, end + 1):
+    return simple_list(start, end)
+
+
+def simple_list(start, end):
+    simp_num = []
+    for num in range(start, end + 1):
         k = 0
-        if i == 1:
+        if num == 1:
             k = 1
-        for j in range(2, i // 2 + 1):
-            if i % j == 0:
+        for j in range(2, num // 2 + 1):
+            if num % j == 0:
                 k = 1
         if k < 1:
-            simp_num.append(i)
+            simp_num.append(num)
+    return simp_num
 
-    print(simp_num)
 
+# 6. Вывести таблицу умножения
 
 def task_seventeen():
     task_seventeen_part_two(1, 5)
@@ -166,6 +238,9 @@ def task_seventeen_part_two(f_num, l_num):
 
 # Topic "Files"
 
+# Напишите функцию read_last(lines, file), которая будет открывать определенный
+# файл file и выводить на печать построчно последние строки в количестве lines (на
+# всякий случай проверим, что задано положительное целое число).
 
 def task_eighteen():
     file_data = input("Enter the path to the file and the number of lines: \n\t").split(" ")
@@ -187,12 +262,16 @@ def read_last(lines, file_name):
         print("Not such file or directory!")
 
 
+# Выберите любую папку на своем компьютере, имеющую вложенные директории.
+# Выведите на печать в терминал ее содержимое, как и всех подкаталогов при
+# помощи функции print_docs(directory).
+
 def print_docs(directory, lvl=0):
     for i in os.listdir(directory):
         f_path = "\n" + directory + "\\" + i
         if os.path.isdir(f_path):
-            print('\t' * (lvl+1), f_path, ":")
-            print_docs(f_path, lvl+1)
+            print('\t' * (lvl + 1), f_path, ":")
+            print_docs(f_path, lvl + 1)
         else:
             print('\t' * lvl, i)
 
@@ -205,6 +284,9 @@ def print_docs_var_2(directory):
         for i in files:
             print(f"\t{i}")
 
+
+# Требуется реализовать функцию longest_words(file), которая выводит слово,
+# имеющее максимальную длину (или список слов, если таковых несколько).
 
 def longest_words(file):
     try:
@@ -241,5 +323,71 @@ def search_for_similar(word_list, word_index):
     return words
 
 
+# Требуется создать csv-файл «rows_300.csv» со следующими столбцами:
+# – № - номер по порядку (от 1 до 300);
+# – Секунда – текущая секунда на вашем ПК;
+# – Микросекунда – текущая миллисекунда на часах.
+# На каждой итерации цикла искусственно приостанавливайте скрипт на 0,01 секунды.
+
+def row_csv(filename='rows_300.csv'):
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", lineterminator="\r")
+        count = 0
+        for i in range(301):
+            if count == 0:
+                writer.writerow(["№", "Секунда", "Миллисекунда"])
+                count += 1
+            else:
+                sec = datetime.datetime.now().second
+                m_sec = datetime.datetime.now().microsecond
+                writer.writerow([i, sec, m_sec])
+                time.sleep(0.01)
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            print(' | '.join(row))
+
+
+# Hard Tasks
+
+# Напишите функцию, которая будет принимать список чисел и возвращать
+# «Boom!», если в списке встречается цифра 7. В противном случае функция
+# должна вернуть «there is no 7 in the list».
+def boom(num_list):
+    for num in num_list:
+        try:
+            if int(num) == 7:
+                return "BOOM!"
+        except ValueError:
+            continue
+    else:
+        return "There is no 7 in the list"
+
+
+# Самое длинное слово
+# Напишите функцию, которая будет находить самое длинное слово в
+# предложении. Если будет найдено два и больше слов одинаковой длины,
+# нужно вернуть первое из них.
+def longest_word_in_string(sentence):
+    word_list = sentence.split(" ")
+    search_max = max(word_list, key=len)
+    return search_max
+
+
+# Наибольшее простое число в диапазоне
+# Напишите функцию, принимающую начальное и конечное значения диапазона
+# чисел и возвращающую наибольшее простое число в этом диапазоне.
+def max_simple_num(start, end):
+    return max(simple_list(start, end))
+
+
+# Сумма диапазона чисел
+# Напишите функцию, которая будет принимать начальное и конечное число в
+# диапазоне чисел и возвращать сумму всех чисел этого диапазона.
+def sum_range(min_num, max_num):
+    return sum(range(min_num, max_num+1))
+
+
 if __name__ == "__main__":
-    longest_words_var_2("C:\\Users\\Aleks\\Desktop\\Дэн\\123.txt")
+    pass
